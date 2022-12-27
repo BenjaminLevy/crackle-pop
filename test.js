2fa98443e2eaa26d1af78e028ad867c20ae80f50
Benjamin <BnLvy1@gmail.com>
2022-12-27 14:39:16 -0800

    wip

--- test.js
+++ test.js
@@ -1,25 +1,9 @@
 const { execSync } = require('node:child_process');
 
-let results
 
-const main = async () => {
-  try{
-  execSync('node crackle-pop.js', (error, stdout, stderr) => {
-    console.log('running crackle-pop.js')
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return error;
-    }
-    results = stdout
-    // console.log(typeof results)
-    return "hello"
-  });
-  } catch (e){
-    return e
-  }
-}
+const res = execSync('node crackle-pop.js')
 
-console.log(main())
+console.log(res)
 
 setInterval(console.log, 5000, "hi")
 // const resultsArr = results.split("\n")
